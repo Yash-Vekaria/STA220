250f60d79602ee2193187af0ac05eef11d81d125
Yash Vekaria <yvekaria.09@gmail.com>
2024-03-20 19:12:44 -0700

    Bug fixes

--- Spider_code/Crawler/spiders/YelpScraper.py
+++ Spider_code/Crawler/spiders/YelpScraper.py
@@ -11,7 +11,7 @@ class YelpScraper(scrapy.Spider):
     name = "YelpScraper"
     scrap_reviews = True
 
-#Function to take the list of cities to scrap data.
+    # Function to take the list of cities to scrap data.
     def start_requests(self):
         cities = []
         curr_path = os.path.abspath(os.path.join("..", "Stored_Info", "Locations", "CA.csv"))
@@ -20,7 +20,7 @@ class YelpScraper(scrapy.Spider):
             for line in f:
                 cities.append(line.strip())
         
-#Constructing the URL and preparing it for the spider to crawl
+        # Constructing the URL and preparing it for the spider to crawl
         url_start = 'https://www.yelp.com/search?'
         for city in cities[:12]:  
             self.logger.info(f"city is {city}")
@@ -31,7 +31,7 @@ class YelpScraper(scrapy.Spider):
             self.logger.info(f"Constructed URL: {url}")
             yield scrapy.Request(url, meta={"city": city, "first_page": True}, callback=self.parse)
 
-#Function to parse the list of restaurants url
+    # Function to parse the list of restaurants url
     def parse(self, response):
         self.logger.debug(f"Meta content: {response.meta}")
         city = response.meta["city"]
@@ -41,10 +41,10 @@ class YelpScraper(scrapy.Spider):
             item["City"] = city
             item["Name"] = "Creating_Required"
             yield item
-            #self.logger.info(f"Data for first page - City: {item['City']}, Name: {item['Name']}")
+            # self.logger.info(f"Data for first page - City: {item['City']}, Name: {item['Name']}")
             
         res = scrapy.Selector(response)
-        #self.logger.info(f"SCRAPPY: {res}")
+        # self.logger.info(f"SCRAPPY: {res}")
         place_urls = res.xpath("//h3/span[text()[contains(., '.')]]/a/@href").extract()
         self.logger.info(f"NeXT ONE:{place_urls}")
         for place_url in place_urls:
@@ -55,7 +55,8 @@ class YelpScraper(scrapy.Spider):
             f.write(f"{url}\n")
             f.close()
             yield scrapy.Request(url, meta={"city": city}, callback=self.parse2)
-#Code snippet to check if a perticular web page has link to go to next page of restaurants
+        
+        # Code snippet to check if a perticular web page has link to go to next page of restaurants
         try:
             next_link = res.xpath("//a[contains(@class, 'next-link')]/@href").extract_first()
             #next_url = "https://www.yelp.com" + next_link
@@ -65,14 +66,14 @@ class YelpScraper(scrapy.Spider):
         except TypeError:
             pass
 
-#Function to crawl and parse the information on each restaurant's page
+    # Function to crawl and parse the information on each restaurant's page
     def parse2(self, response):
-        #self.logger.debug(f"Meta content: {response.meta}")
+        # self.logger.debug(f"Meta content: {response.meta}")
         place = scrapy.Selector(response)
-        #self.logger.debug(f"theplace: {place}")
+        # self.logger.debug(f"theplace: {place}")
         item = YelpItem()
 
-        #Information scraped from each website
+        # Information scraped from each website
         item["City"] = response.meta["city"]
         item["Name"] = place.xpath("//h1[@class='css-hnttcw']/text()").get()
         self.logger.info(f"NAMEIS:{item['Name']}")
